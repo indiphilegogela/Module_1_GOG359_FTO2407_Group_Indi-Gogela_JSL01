608a9e7941790dec3c813b6eacb83477ed912394
indiphilegogela <156888615+indiphilegogela@users.noreply.github.com>
2024-09-01 13:15:09 +0200

    Final Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
+    let regEx = /pet_[A-Z 0-9]+$/i;
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
+        if (regEx.test(input)){
+            result = "🟩Valid Syntax";
+        } else {
+            result = "🟥Invalid Syntax";
+        }
             document.getElementById('result').innerText = result;
 }
-
-
